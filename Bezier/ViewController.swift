8aba0f59c36b7a3aa295f7540772056f06eec9e5
Carlos <csalatti@gmail.com>
2017-08-22 10:23:22 -0400

    22Aug 10:23AM

--- Bezier/ViewController.swift
+++ Bezier/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     //------------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
+        angle.text = String(0.0)
         fillArrayOfAngles()
     }
     //------------------------------------
